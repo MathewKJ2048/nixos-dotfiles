526864783eec0b6783d39db22afbceb5dd70dc9c
Mathew Kuthur James <mathewkj2048@gmail.com>
2025-10-08 14:47:34 -0400

    switched to stable

--- README.md
+++ README.md
@@ -20,6 +20,12 @@ sudo nixos-rebuild switch --flake .#nixos   # this takes some time, the hostname
 Go to the directory where the flake.lock file is present. 
 
 ```
-nix flake update   # updates the flake.lock file to the latest
+sudo nix flake update   # updates the flake.lock file to the latest
 sudo nixos-rebuild switch --flake .#nixos       # syncs the system with the new flake.lock file
+```
+
+After multiple updates, if the disk space is being used up, run:
+
+```
+nix-garbage-collect
 ```
--- flake.nix
+++ flake.nix
@@ -2,16 +2,17 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05"; # channel to draw from
   };
 
   
-  outputs = { self, nixpkgs, ... }@inputs: 
+  outputs = { self, nixpkgs, ... }@inputs: # {x,y}@a means that I can access x via a.x
   
   {
-
+    # the string "nixos" represents the hostname (which is distinct from the username)
+    # nixosConfigurations is the attribute that nixos-rebuild looks for in a flake (other nix commands look for different things)
     nixosConfigurations."nixos" = nixpkgs.lib.nixosSystem {
-    modules = [ ./configuration.nix ];
+    modules = [ ./configuration.nix ]; # no need to import hardware-configurations as well because it's imported in configurations.nix already
    };
 
   };
