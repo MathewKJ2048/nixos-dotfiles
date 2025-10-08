98098e1b965e67482dde852efc262d964eec10e0
Mathew Kuthur James <mathewkj2048@gmail.com>
2025-10-08 03:36:39 -0400

    let keyword for flake

--- flake.nix
+++ flake.nix
@@ -5,11 +5,16 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: {
+  
+  outputs = { self, nixpkgs }: 
+  let
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  in
+  {
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+    packages.x86_64-linux.hello = pkgs.hello;
 
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+    packages.x86_64-linux.default = pkgs.hello;
 
   };
 }
