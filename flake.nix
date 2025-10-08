fbe2a6c9b035fd08b7b1f6e7937a1c41e94e8fa9
Mathew Kuthur James <mathewkj2048@gmail.com>
2025-10-08 03:07:19 -0400

    first flake

--- flake.nix
+++ flake.nix
@@ -1,21 +1,15 @@
 {
-    description = "My first flake";
+  description = "A very basic flake";
 
-    inputs = {
-        nixpkgs.url = "nixpkgs/nixos-23.05";
-    };
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
 
-    outputs = {self, nixpkgs, ...} :
-        let
-            lib = nixpkgs.lib;
-        in
-        {
-            nixosConfigurations = {
-                nixos = lib.nixosSystem {
-                    system = "x86_64-linux";
-                    modules = [./configuration.nix]
-                };
-            };
-        };
+  outputs = { self, nixpkgs }: {
 
+    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+
+    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+
+  };
 }
