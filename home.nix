0d2e3e041d307eba5fe2be4951981e6e9df6cb49
Mathew Kuthur James <mathewkj2048@gmail.com>
2025-10-17 13:46:34 -0400

    aesthetics

--- home.nix
+++ home.nix
@@ -6,14 +6,27 @@
     home.stateVersion = "25.05";
 
     home.packages = with pkgs; [
-        sl
-        guake
-        vlc
-        vscodium
-        fzf
-        zoxide
-        dconf2nix
-        # flameshot
+        
+        guake # drop-down terminal
+        vscodium # code editing
+        vlc # music and video
+        # flameshot screenshots
+        
+        dconf2nix # tool used to configure dconf dumps, move it to a shell maybe?
+        
+        # shell quality of life
+        zoxide # better cd
+        fzf # fuzzy-finding
+        ripgrep # not aliased because it's not a drop-in replacement for grep, but it's much faster
+
+        # purely for aesthetics
+        btop # monitor performance
+        cava # audio
+        fastfetch # system information
+        sl # steam locomotive
+        cbonsai # grow bonsai on the terminal
+        cowsay # make an ascii cow say things
+        cmatrix # make the terminal resemble The Matrix
     ];
 
     imports = [
@@ -29,16 +42,21 @@
         ./apps/x.nix
     ];
 
-    systemd.user.services.drop-down-terminal = {
+    
+    systemd.user.services.drop-down-terminal = 
+    let
+        target = "graphical-session.target";
+    in
+    {
         Unit = {
             Description = "automatically start drop-down terminal guake";
-            After = ["graphical-session.target"]; # wait for cinnamon to load first
-            Requires = ["graphical-session.target"]; # dependency: if the desktop stops, so does guake
+            After = [target]; # wait for cinnamon to load first
+            Requires = [target]; # dependency: if the desktop stops, so does guake
         };
-        Install.WantedBy = ["graphical-session.target"];
+        Install.WantedBy = [target];
         Service = {
             Type = "simple"; # most common type
-            ExecStart = "${pkgs.guake}/bin/guake"; # a simple command won't work because the shell is loaded after systemd
+            ExecStart = "${pkgs.guake}/bin/guake --hide"; # a simple command won't work because the shell is loaded after systemd, --hide is used to prevent opening it, and also introduces a slight delay so that it doesn't open before x and cinnamon are fully loaded
         };
     };
 
